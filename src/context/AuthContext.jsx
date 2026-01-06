ba217659c1125db47dd9681cb2c96059d20616c3
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-06 15:36:23 +0530

    login page

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,11 +1,18 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import API from "../api/axios";
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
 
   // Load user from token on app start
   useEffect(() => {
@@ -27,9 +34,13 @@ export const AuthProvider = ({ children }) => {
 
   // Login function
   const login = async (email, password) => {
-    const res = await API.post("/auth/login", { email, password });
-    localStorage.setItem("token", res.data.token);
-    setUser(res.data.user);
+    const { data} = await API.post("/auth/login", {
+       email, 
+       password
+     });
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("user", JSON.stringify(data.user));
+    setUser(data.user);
   };
 
   // Logout function
