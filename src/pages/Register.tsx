69c38b198b4d82358b5040fab359da44674499ea
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-06 18:30:03 +0530

    register done

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,2 +1,59 @@
 import { useState } from "react";
- 
+import API from "../api/axios";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await API.post("/auth/register", {
+        name,
+        email,
+        password,
+      });
+
+      alert("Registration successful. Please login.");
+      navigate("/login");
+    } catch (error) {
+      alert("Registration failed");
+    }
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <h2>Register</h2>
+
+      <input
+        type="text"
+        placeholder="Name"
+        required
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <input
+        type="email"
+        placeholder="Email"
+        required
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        required
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;
